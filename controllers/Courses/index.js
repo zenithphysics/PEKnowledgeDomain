ffd4c6309554f68fccd06a72bdd9f1b8dd5d6e93
Sushant Singh <sushantr134@gmail.com>
2019-10-09 21:36:51 +0530

    Controllers added for coursesAdmin | joi validation need to be added

---
+++ controllers/Courses/index.js
@@ -0,0 +1,101 @@
+const Course = require("../../models/courses/course").model;
+const Subject = require("../../models/courses/subject").model;
+const Chapter = require("../../models/courses/chapter").model;
+
+exports.addCourse = (req, res) => {
+  //here need to write joi validation check for req.body
+  const { CourseTitle, Created_by, Description, Video_link } = req.body;
+  const newCourse = {
+    CourseTitle,
+    Created_by,
+    Description,
+    Video_link
+  };
+  new Course(newCourse)
+    .save()
+    .then(newcourse => {
+      res.status(200).json({ ...newcourse._doc, message: "Course Added Successfully", status: 200 });
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+};
+
+exports.addSubject = (req, res) => {
+  Course.findOne({ CourseTitle: req.body.CourseTitle }).then(course => {
+    if (!course) {
+      res.status(404).json({ message: "No such course found!!!", status: 404 });
+    }
+    const { SubjectTitle, Description, CourseTitle } = req.body;
+
+    const newSubject = {
+      courseId: course._id,
+      SubjectTitle,
+      Description,
+      CourseTitle
+    };
+    new Subject(newSubject)
+      .save()
+      .then(subject => {
+        Subject.findOne({ CourseTitle: req.body.CourseTitle })
+          .then(subject => {
+            Course.findOneAndUpdate({ CourseTitle: req.body.CourseTitle }, { $push: { Subjects: subject._id } }, { new: true })
+              .then(data => res.json(data))
+              .catch(err => console.log(err));
+          })
+          .catch(err => console.log(err));
+      })
+      .catch(err => console.log(err));
+  });
+};
+
+exports.addChapter = (req, res) => {
+  Subject.findOne({ SubjectTitle: req.body.SubjectTitle }).then(subject => {
+    if (!subject) {
+      res.status(400).json("No subject found!!");
+    }
+    const newChapter = {
+      subjectTitle: req.body.subjectTitle,
+      chapterTitle: req.body.chapterTitle,
+      description: req.body.chapterDescription,
+      subjectId: subject._id
+    };
+    new Chapter(newChapter)
+      .save()
+      .then(chapter => {
+        Chapter.findOne({ subjectTitle: req.body.subjectTitle })
+          .then(chapter => {
+            Subject.findOneAndUpdate({ subjectTitle: req.body.subjectTitle }, { $push: { Chapters: chapter._id } }, { new: true })
+              .then(data => res.json(data))
+              .catch(err => console.log(err));
+          })
+          .catch(err => console.log(err));
+      })
+      .catch(err => console.log(err));
+  });
+};
+
+exports.addTopic = (req, res) => {
+  Chapter.findOne({ chapterTitle: req.body.chapterTitle }).then(chapter => {
+    if (!chapter) {
+      res.status(400).json("no chapter found");
+    }
+    const newTopic = {
+      topicTitle: req.body.topicTitle,
+      chapterId: chapter._id,
+      Description: req.body.Description
+    };
+    new Topic(newTopic)
+      .save()
+      .then(topic => {
+        Topic.findOne({ topictitle: req.body.chapterTitle })
+          .then(topic => {
+            Chapter.findOneAndUpdate({ chapterTitle: req.body.chapterTitle }, { $push: { Topics: topic._id } }, { new: true })
+              .then(data => res.json(data))
+              .catch(err => console.log(err));
+          })
+          .catch(err => console.log(err));
+      })
+      .catch(err => console.log(err));
+  });
+};
--- routes/course-admin.js
+++ routes/course-admin.js
@@ -1,113 +1,19 @@
-const router = require('express').Router()
-const bodyParser = require('body-parser')
-const Course = require('../models/courses/course').model
-const Subject = require('../models/courses/subject').model
-const Chapter = require('../models/courses/chapter').model
-const mongoose= require('mongoose')
+const router = require("express").Router();
 
+const { addCourse, addSubject, addChapter, addTopic } = require("../controllers/Courses");
 
+/* important please use camelCase notation for variables and attributes should have First letter in uppercase  */
 
 //-----------------add course post route ----------//
-router.post('/addcourse' , (req,res)=> {
-
-    console.log(req.body)
-          const newCourse = {
-              CourseTitle:req.body.CourseTitle,
-              created_by:req.body.created_by,
-              Description:req.body.Description,
-              video_link:req.body.video_link,
-
-          }
-          new Course(newCourse).save()
-          .then((newcourse)=> {
-                res.status(200).json(newcourse)
-          })
-          .catch((err)=> {
-                res.status(400).json(err)  
-          })
-          
-})
+router.post("/addcourse", addCourse);
 
 //---------------add subjects post router -----------------//
-router.post('/addsubject' , (req,res)=> {
-           Course.findOne({CourseTitle:req.body.CourseTitle})
-           .then((course)=> {
-            if (!course) {
-                res.status(404).json({ err: "No course found!!!" });
-              }
-              
-              const newSubject = {
-                courseId:course._id,
-                SubjectTitle:req.body.SubjectTitle,
-                Description:req.body.Description,
-                CourseTitle:req.body.CourseTitle,
-                
-              }
-              new Subject(newSubject).save()
-              .then((subject)=> {
-                   Subject.findOne({CourseTitle:req.body.CourseTitle})
-                   .then((subject)=> {
-                         Course.findOneAndUpdate({ CourseTitle: req.body.CourseTitle }, { $push: { Subjects: subject._id } }, { new: true })
-                         .then(data => res.json(data))
-                         .catch(err => console.log(err));
-                   })
-                   .catch(err => console.log(err));
-              })
-              .catch(err => console.log(err));
-           })
-})
+router.post("/addsubject", addSubject);
 
 //----------add chapter post route--------------------//
-router.get('/addchapter' , (req,res)=> {
-   Subject.findOne({SubjectTitle:req.body.SubjectTitle})
-   .then((subject)=> {
-              if(!subject) {
-                  res.status(400).json('No subject found!!')
-              }
-              const newChapter = {
-                subjectTitle: req.body.subjectTitle,
-                chapterTitle: req.body.chapterTitle,
-                description: req.body.chapterDescription,
-                subjectId: subject._id
-              }
-              new Chapter(newChapter).save()
-              .then((chapter)=> {
-                       Chapter.findOne({subjectTitle:req.body.subjectTitle})
-                       .then((chapter)=> {
-                        Subject.findOneAndUpdate({ subjectTitle: req.body.subjectTitle }, { $push: { Chapters: chapter._id } }, { new: true })
-                        .then(data => res.json(data))
-                        .catch(err => console.log(err));
-                       })
-                       .catch(err => console.log(err));
-              })
-              .catch(err => console.log(err));
-   })
-})
+router.get("/addchapter", addChapter);
 
 //-----------------add topic post route --------------//
- router.post('/addtopic' , (req,res)=> {
-     Chapter.findOne({chapterTitle:req.body.chapterTitle})
-     .then((chapter)=> {
-                 if(!chapter){
-                     res.status(400).json('no chapter found')
-                 }
-                 const newTopic = {
-                    topicTitle:req.body.topicTitle,
-                    chapterId:chapter._id,
-                    Description:req.body.Description,
-                }
-              new Topic(newTopic).save()
-              .then((topic)=> {
-                Topic.findOne({ topictitle: req.body.chapterTitle })
-                .then((topic)=> {
-                    Chapter.findOneAndUpdate({ chapterTitle: req.body.chapterTitle }, { $push: { Topics: topic._id } }, { new: true })
-                    .then(data => res.json(data))
-              .catch(err => console.log(err));
-                })
-                .catch(err => console.log(err));
-              })
-              .catch(err => console.log(err));
-     })
- })
+router.post("/addtopic", addTopic);
 
-module.exports = router
+module.exports = router;
