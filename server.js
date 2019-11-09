8c5bd445abcefc2c6abc72461220fdc0caad1d6c
DEEPAK MALIK <deepakm661@gmail.com>
2019-11-09 16:52:57 +0530

    url changed

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 mongoose
-  .connect( string(keys.mongodb.dbURI) , { useUnifiedTopology: true, useNewUrlParser: true })
+  .connect( 'mongodb+srv://deepak:deepak123@cluster0-6rn6a.mongodb.net/test?retryWrites=true&w=majority' , { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log( 'connected to mongodb'))
   .catch(err => {
     console.log(err);
